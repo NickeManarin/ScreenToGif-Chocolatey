700d183db67ed9d14b3d0e22a6359bdf95572479
Nicke Manarin <nicke@outlook.com.br>
2016-07-03 23:07:49 -0300

    Updated version

--- screentogif/tools/chocolateyInstall.ps1
+++ screentogif/tools/chocolateyInstall.ps1
@@ -1,12 +1,12 @@
 $tools = Split-Path $MyInvocation.MyCommand.Definition
 $content = Join-Path (Split-Path $tools) 'content'
 $shortcut = Join-Path ([System.Environment]::GetFolderPath('CommonPrograms')) 'ScreenToGif.lnk'
-$exe = Join-Path $content 'ScreenToGif 1.4.2.exe'
+$exe = Join-Path $content 'ScreenToGif 2.exe'
 
 Install-ChocolateyZipPackage `
     -PackageName 'screentogif' `
-    -Url 'http://download-codeplex.sec.s-msft.com/Download/Release?ProjectName=screentogif&DownloadId=1480830&FileTime=130851210516500000&Build=21031' `
-    -Checksum '825964A156FC30710CF5A98EBAB9E354016E8A4DDEB52936C08D51E92CA3E193' `
+    -Url 'http://download-codeplex.sec.s-msft.com/Download/Release?ProjectName=screentogif&DownloadId=1572748&FileTime=131101759333270000&Build=21031' `
+    -Checksum '8f9db5b99c1c95ced26886992be0ec44d9512b7ab46bef3f18d5a29c7fb5e3ca' `
     -ChecksumType 'SHA256'
     -UnzipLocation $content
 
