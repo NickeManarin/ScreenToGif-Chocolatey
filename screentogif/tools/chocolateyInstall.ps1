3f151a3a3b64ce439e1d4262dd81bba9ba9cfbc6
Nicke Manarin <nicke@outlook.com.br>
2016-08-02 20:52:14 -0300

    Checksum...

--- screentogif/tools/chocolateyInstall.ps1
+++ screentogif/tools/chocolateyInstall.ps1
@@ -6,7 +6,7 @@ $exe = Join-Path $content 'ScreenToGif 2.1.exe'
 Install-ChocolateyZipPackage `
     -PackageName 'screentogif' `
     -Url 'http://download-codeplex.sec.s-msft.com/Download/Release?ProjectName=screentogif&DownloadId=1597782&FileTime=131144970380730000&Build=21031' `
-    -Checksum '65BF9F9B8F17B2C2B85720F6F4DD4A35561A9554A217183A3F8D95E390114F14' `
+    -Checksum '2EC4F2FB087EC08E2EAB600CCDE04445081D7F197FC5C4EC424C54CCA90687FB' `
     -ChecksumType 'SHA256' `
     -UnzipLocation $content
 
