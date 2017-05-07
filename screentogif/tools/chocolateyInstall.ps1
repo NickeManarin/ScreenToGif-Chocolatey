ff60a569fa853e844f211a38dbb840d5c36301d6
Nicke Manarin <nicke@outlook.com.br>
2017-05-06 23:09:02 -0300

    v2.7.2

--- screentogif/tools/chocolateyInstall.ps1
+++ screentogif/tools/chocolateyInstall.ps1
@@ -5,8 +5,8 @@ $exe = Join-Path $content 'ScreenToGif.exe'
 
 Install-ChocolateyZipPackage `
     -PackageName 'screentogif' `
-    -Url 'https://github.com/NickeManarin/ScreenToGif/releases/download/2.7.1/ScreenToGif.2.7.1.zip' `
-    -Checksum '36D513782658755E24EFC9885152A303989C7464CF832BBE0DEB2E4A911D1550' `
+    -Url 'https://github.com/NickeManarin/ScreenToGif/releases/download/2.7.2/ScreenToGif.2.7.2.zip' `
+    -Checksum '9D44814291A45BA9EF0150D61FEA1CEFFCF4C37E0A87256D747DA4372C4CE443' `
     -ChecksumType 'SHA256' `
     -UnzipLocation $content
 
