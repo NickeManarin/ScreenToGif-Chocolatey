5f4cf0424a28c72e19a7530d28dccf5163188894
Nicke Manarin <nicke@outlook.com.br>
2020-02-25 17:18:05 -0300

    Version 2.20.4

--- screentogif/tools/chocolateyInstall.ps1
+++ screentogif/tools/chocolateyInstall.ps1
@@ -5,8 +5,8 @@ $exe = Join-Path $content 'ScreenToGif.exe'
 
 Install-ChocolateyZipPackage `
     -PackageName 'screentogif' `
-    -Url 'https://github.com/NickeManarin/ScreenToGif/releases/download/2.20.3/ScreenToGif.2.20.3.Portable.zip' `
-    -Checksum 'D09D77CFC9658481C4B7C49687E6C2DFA2E9D5B1B2CA12DA8E7435603F785E78' `
+    -Url 'https://github.com/NickeManarin/ScreenToGif/releases/download/2.20.4/ScreenToGif.2.20.4.Portable.zip' `
+    -Checksum '99757471EE14933FF216F26702A95EF54DAFF28627D597C5BCD4D375EDEBB008' `
     -ChecksumType 'SHA256' `
     -UnzipLocation $content
 
